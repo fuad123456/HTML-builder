df24925067a5aed5bb85c22100f8182d9a7e2380
Fuad Крым <fuadkrym@gmail.com>
2022-05-24 01:35:50 +0300

    refactor: rename

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,13 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const { mkdir, readdir, access, copyFile, rm, appendFile } = require('fs/promises');
 
-let destPath = path.join(__dirname, 'project-dist');
+let tmp = path.join(__dirname, 'template.html');
 let componentsPath = path.join(__dirname, 'components');
-let templatePath = path.join(__dirname, 'template.html');
 let assetsPath = path.join(__dirname, 'assets');
-let destAssetsPath = path.join(destPath, 'assets');
-let stylesPath = path.join(__dirname, 'styles');
-let destStylesPath = path.join(path.join(destPath, 'style.css'));
+let destPath = path.join(__dirname, 'project-dist');
+let cssPath = path.join(__dirname, 'styles');
+let destcssPath = path.join(path.join(destPath, 'style.css'));
+let assets = path.join(destPath, 'assets');
 let mp = {};
 
 let readComponents = async function() {
@@ -16,9 +16,9 @@ let readComponents = async function() {
     for (let file of files) {
       if (file.isFile() && path.extname(file.name) === '.html') {
         let input = await fs.createReadStream(path.join(componentsPath, file.name), 'utf-8');
-        input.on('data', (chunk) => {
+        input.on('data', (ch) => {
           let key = `{{${ path.parse(file.name).name }}}`;
-          mp[key] = mp[key] ? mp[key] += chunk : chunk;
+          mp[key] = mp[key] ? mp[key] += ch : ch;
         });
       }
     }
@@ -26,51 +26,51 @@ let readComponents = async function() {
 
 let buildHTML = async function() {
   await readComponents();
-  let result = '';
-  let input = await fs.createReadStream(templatePath, 'utf-8');
-  input.on('data', (chunk) => result += chunk);
+  let res = '';
+  let input = await fs.createReadStream(tmp, 'utf-8');
+  input.on('data', (ch) => res += ch);
   input.on('error', (err) => console.error(err));
   input.on('end', () => {
     for (let key in mp) {
-      result = result.split(key).join(mp[key]);
+      res = res.split(key).join(mp[key]);
     }
     let output = fs.createWriteStream(path.join(destPath, 'index.html'));
-    output.write(result);
+    output.write(res);
   });
 };
 
 let mergeOfStyles = async function() {
-  fs.createWriteStream(destStylesPath);
-    let files = await readdir(stylesPath, {withFileTypes: true});
+  fs.createWriteStream(destcssPath);
+    let files = await readdir(cssPath, {withFileTypes: true});
     for (let file of files) {
       if (file.isFile() && path.extname(file.name) === '.css') {
-        let input = await fs.createReadStream(path.join(stylesPath, file.name), 'utf-8');
-        input.on('data', (chunk) => appendFile(destStylesPath, chunk));
+        let input = await fs.createReadStream(path.join(cssPath, file.name), 'utf-8');
+        input.on('data', (ch) => appendFile(destcssPath, ch));
       }
     }
 };
 
-let copyFiles = async function (srcPath, destPath) {
+let copy = async function (srcPath, destPath) {
     let dir = await readdir(srcPath, {withFileTypes: true});
     for (let item of dir) {
       if (item.isFile()) {
         await copyFile(path.join(srcPath, item.name), path.join(destPath, item.name));
       } else {
         await mkdir(path.join(destPath, item.name), { recursive: true });
-        copyFiles(path.join(srcPath, item.name), path.join(destPath, item.name));
+        copy(path.join(srcPath, item.name), path.join(destPath, item.name));
       }
     }
 };
 
 let copyAssets = async function() {
   try {
-    await access(destAssetsPath);
-    await rm(destAssetsPath, { recursive: true, force: true });
-    await mkdir(destAssetsPath, { recursive: true });
-    copyFiles(assetsPath, destAssetsPath);
+    await access(assets);
+    await rm(assets, { recursive: true, force: true });
+    await mkdir(assets, { recursive: true });
+    copy(assetsPath, assets);
   } catch {
-    await mkdir(destAssetsPath, { recursive: true });
-    copyFiles(assetsPath, destAssetsPath);
+    await mkdir(assets, { recursive: true });
+    copy(assetsPath, assets);
   }
 };
 
